06b0be887dfccb64fe1692caa997daafaf4622f3
Lilia <lilia@student.dev-island.org>
2024-02-20 18:21:36 -1000

    create pokemon details component

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,37 @@ import PokemonCard from "./PokemonCard";
 import pokidexEntries from "./pokemon";
 
 import "./App.css";
+import PokemonDetails from "./PokemonDetails";
 
 function App() {
   const [pokidex, setPokidex] = useState(pokidexEntries);
+  let [selectedPokemon, setSelectedPokemon] = useState(pokidex[0]);
 
   return (
     <main className="main-container">
       <h1 className="page-header">React Pokidex</h1>
+
+      <div className="selected-pokemon-container">
+        <PokemonDetails
+          name={selectedPokemon.name}
+          type={selectedPokemon.type}
+          icon={selectedPokemon.icon}
+          desription={selectedPokemon.description}
+        />
+      </div>
 
       <div className="pokemon-cards-container">
         {pokidex &&
           pokidex.map((pokemon) => {
             return (
               <PokemonCard
+                key={pokemon.name}
                 name={pokemon.name}
                 icon={pokemon.icon}
                 typeIcon={pokemon.typeIcon}
+                onClickHandler={() => {
+                  setSelectedPokemon(pokemon);
+                }}
               />
             );
           })}
---
+++ src/PokemonDetails.jsx
@@ -0,0 +1,11 @@
+
+export default function PokemonDetails({ name, icon, type, desription }) {
+    return (
+      <div className="selected-pokemon-card">
+        <h2>{name}</h2>
+        <p>Type:{type}</p>
+        <img className="selected-pokemon-pic" src={icon} alt="" />
+        <p>Description:{desription}</p>
+      </div>
+    );
+  }
