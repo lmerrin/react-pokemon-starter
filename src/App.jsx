18097518dc1c45b29e718b8e38fe37f0e9160b7f
Lilia <lilia@student.dev-island.org>
2024-02-20 19:08:12 -1000

    create poke card collection component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
-import PokemonCard from "./PokemonCard";
 import pokidexEntries from "./pokemon";
+import PokemonCardCollection from "./components/PokemonCardCollection";
 
 import "./App.css";
-import PokemonDetails from "./PokemonDetails";
+import PokemonDetails from "./components/PokemonDetails";
 
 function App() {
   const [pokidex, setPokidex] = useState(pokidexEntries);
@@ -22,22 +22,10 @@ function App() {
         />
       </div>
 
-      <div className="pokemon-cards-container">
-        {pokidex &&
-          pokidex.map((pokemon) => {
-            return (
-              <PokemonCard
-                key={pokemon.name}
-                name={pokemon.name}
-                icon={pokemon.icon}
-                typeIcon={pokemon.typeIcon}
-                onClickHandler={() => {
-                  setSelectedPokemon(pokemon);
-                }}
-              />
-            );
-          })}
-      </div>
+      <PokemonCardCollection
+        pokidex={pokidex}
+        onPokemonCardClick={setSelectedPokemon}
+      />
     </main>
   );
 }
